910d55e1902571cedc4906edcc084f3555bc2061
Zack Stout <zackstout@gmail.com>
2018-03-07 00:14:02 -0600

    oh how strange, python is running consistently faster than python3

--- runner.py
+++ runner.py
@@ -1,1 +1,16 @@
 
+import time
+nums = []
+
+start = int(round(time.time()*1000))
+# print(start)
+for i in range(0, 1000):
+    for j in range(0, 1000):
+        nums.append(i * 1000 + j)
+end = int(round(time.time()*1000))
+# print(end)
+
+# It seems that this returns seconds:
+print(time.time())
+
+print(end - start)
